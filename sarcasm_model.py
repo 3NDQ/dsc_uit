23bbbfc837c02e5da59f92e9a82487fe1499ee8c
MinSun05 <minhnhut12052004@gmail.com>
2025-01-18 22:02:32 +0700

    update

--- sarcasm_model.py
+++ sarcasm_model.py
@@ -111,8 +111,9 @@ class VietnameseSarcasmClassifier(nn.Module):
         alpha = [a / alpha_sum for a in alpha]
 
         if labels is not None:
-            device = final_logits.device  # Lấy thiết bị của final_logits
-            labels = labels.to(device)
+            print(alpha.device, final_logits.device, labels.device)
+            # device = final_logits.device  # Lấy thiết bị của final_logits
+            # labels = labels.to(device)
             criterion = FocalLoss(alpha=alpha, gamma=2, reduction="mean")
             loss = criterion(final_logits, labels)
 
