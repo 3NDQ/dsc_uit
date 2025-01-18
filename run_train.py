6556bc468729b2d6e2c3783afa91fd9fda1109e9
Gad <faigar1004@gmail.com>
2025-01-18 22:10:57 +0700

    Temp update

--- run_train.py
+++ run_train.py
@@ -24,7 +24,7 @@ def train_model(model, train_dataloader, val_dataloader, device, num_epochs, pat
     )
 
     early_stopping = EarlyStopping(patience=patience)
-    scaler = torch.amp.GradScaler(device_type="cuda")
+    scaler = torch.amp.GradScaler()
     
     best_models = []  # List to store the top 5 models based on F1 score
 
