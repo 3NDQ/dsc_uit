a66d53831fba946cd93520bd4160f370c46fce32
Gad <faigar1004@gmail.com>
2025-01-19 10:24:56 +0700

    Temp update

--- sarcasm_model.py
+++ sarcasm_model.py
@@ -26,20 +26,21 @@ class VietnameseSarcasmClassifier(nn.Module):
             self.text_to_image_attention = CrossAttention(d_in=768, d_out_kq=768, d_out_v=768)
             self.image_to_text_attention = CrossAttention(d_in=768 + 768, d_out_kq=768 + 768, d_out_v=768 + 768)
         elif self.fusion_method == 'attention':
-            self.self_attention = SelfAttention(d_in=768+768+ 768, d_out_kq=768+768+768, d_out_v=768 + 768 + 768)
-            
+            self.self_attention = SelfAttention(d_in=768 + 768 + 768, d_out_kq=768+768+768, d_out_v=768 + 768 + 768)
         # Define the output layer
         combined_size = 0
         if self.fusion_method == 'concat':
           combined_size = 768 + 768 + 768 
         elif self.fusion_method == 'cross_attention':
-          combined_size = 768 + 768 + 768+768
+          combined_size = 768 + 768 + 768 + 768
         elif self.fusion_method == 'attention':
           combined_size = 768 + 768 + 768
         self.fc = nn.Linear(combined_size, num_labels)
         self.loss_fct = FocalLoss(gamma=self.gamma, alpha=class_weight_tensor)
 
     def forward(self, image_features, text_features, labels=None):
+        print(len(image_features))
+        print(len(text_features))
         if self.fusion_method == 'cross_attention':
             attended_text = self.text_to_image_attention(text_features, image_features)
             attended_image = self.image_to_text_attention(image_features, text_features)
@@ -48,13 +49,12 @@ class VietnameseSarcasmClassifier(nn.Module):
             combined_features = torch.cat((image_features, text_features), dim=1)
             attended_features = self.self_attention(combined_features)
             combined_features = attended_features
-        elif self.fusion_method == 'mean':
-            combined_features = torch.cat((image_features.mean(), text_features.mean()), dim=1)
         else:
             combined_features = torch.cat((image_features, text_features), dim=1)
-    
+
+        
         logits = self.fc(combined_features)
-    
+
         if labels is not None:
             # Calculate loss using Focal Loss
             loss = self.loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
