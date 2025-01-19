25402927885d9750c89fc16ac56aabd3565da163
Gad <faigar1004@gmail.com>
2025-01-19 22:16:31 +0700

    Temp update

--- sarcasm_model.py
+++ sarcasm_model.py
@@ -1,3 +1,4 @@
+# sarcasm_model.py
 import torch
 import torch.nn as nn
 import logging
@@ -34,20 +35,18 @@ class VietnameseSarcasmClassifier(nn.Module):
         self.text_dense1 = nn.Linear(1024, 1024)
         self.text_dense2 = nn.Linear(1024, 512)
         
-        self.fusion_dense = nn.Linear(3584, 2048)
-        self.fusion_dense1 = nn.Linear(2048, 1024)
-        self.fusion_dense2 = nn.Linear(1024, 512)
-
         # Define attention layers based on fusion method
         if self.fusion_method == 'cross_attention':
-            self.text_to_image_attention = CrossAttention(d_in_q=1024, d_in_kv=2024, d_out_kq=2024, d_out_v=2024)  # d_in for W_query should be 1024 (text_features)
-            self.image_to_text_attention = CrossAttention(d_in_q=2024, d_in_kv=1024, d_out_kq=1024, d_out_v=1024)  # d_in for W_query should be 2024 (image_features)
-            combined_size = 3584
+            self.text_to_image_attention = CrossAttention(d_in_q=1024, d_in_kv=1024, d_out_kq=512, d_out_v=512)
+            self.image_to_text_attention = CrossAttention(d_in_q=1024, d_in_kv=1024, d_out_kq=512, d_out_v=512)
+            self.fusion_dense = nn.Linear(1024, 512)
         elif self.fusion_method == 'attention':
-            self.self_attention = SelfAttention(d_in=3584, d_out_kq=2048, d_out_v=2048)
-            combined_size = 3584
-        else:
-            combined_size = 3584
+            self.self_attention = SelfAttention(d_in=2048, d_out_kq=1024, d_out_v=512)
+            self.fusion_dense = nn.Linear(512, 512)
+        else: # concat
+            self.fusion_dense = nn.Linear(3072, 2048)
+            self.fusion_dense1 = nn.Linear(2048, 1024)
+            self.fusion_dense2 = nn.Linear(1024, 512)
             
         self.fc = nn.Sequential(
             nn.Linear(512, 512 // 2),
@@ -59,7 +58,6 @@ class VietnameseSarcasmClassifier(nn.Module):
         self.loss_fct = FocalLoss(gamma=self.gamma, alpha=self.class_weight) if self.class_weight is not None else FocalLoss(gamma=self.gamma)
 
     def forward(self, image_features, text_features, labels=None):
-        text_features *= 1.5
         
         image_out = self.image_dense1(image_features)
         image_out = nn.ReLU()(image_out)
@@ -73,42 +71,33 @@ class VietnameseSarcasmClassifier(nn.Module):
         text_out1 = nn.ReLU()(text_out1)
         text_out1 = self.dropout(text_out1)
         
-        text_out1 = self.text_dense1(text_features)
-        text_out1 = nn.ReLU()(text_out1)
-        text_out1 = self.dropout(text_out1)
-        
         text_out2 = self.text_dense2(text_features)
         text_out2 = nn.ReLU()(text_out2)
         text_out2 = self.dropout(text_out2)
-        
-        text_out2 = self.text_dense2(text_features)
-        text_out2 = nn.ReLU()(text_out2)
-        text_out2 = self.dropout(text_out2)
-        
-        text_out_combined = torch.cat((text_out1, text_out2, text_features), dim=1)
         
         if self.fusion_method == 'cross_attention':
-            attended_text = self.text_to_image_attention(text_features, image_features)
-            attended_image = self.image_to_text_attention(image_features, text_features)
+            text_out = torch.cat((text_out1, text_out2), dim=1)
+            attended_text = self.text_to_image_attention(text_out, image_out)
+            attended_image = self.image_to_text_attention(image_out, text_out)
             combined_features = torch.cat((attended_text, attended_image), dim=1)
         elif self.fusion_method == 'attention':
-            combined_features = torch.cat((image_features, text_features), dim=1)
-            attended_features = self.self_attention(combined_features)
-            combined_features = attended_features
-        else:
-            combined_features = torch.cat((image_out, text_out_combined), dim=1)
+            combined_features = torch.cat((image_out, text_out1, text_out2), dim=1)
+            combined_features = self.self_attention(combined_features)
+        else: # concat
+            combined_features = torch.cat((image_out, text_out1, text_out2), dim=1)
             
         fusion_out = self.fusion_dense(combined_features)
         fusion_out = nn.ReLU()(fusion_out)
         fusion_out = self.dropout(fusion_out)
         
-        fusion_out = self.fusion_dense1(fusion_out)
-        fusion_out = nn.ReLU()(fusion_out)
-        fusion_out = self.dropout(fusion_out)
+        if self.fusion_method != 'attention' and self.fusion_method != 'cross_attention':
+            fusion_out = self.fusion_dense1(fusion_out)
+            fusion_out = nn.ReLU()(fusion_out)
+            fusion_out = self.dropout(fusion_out)
         
-        fusion_out = self.fusion_dense2(fusion_out)
-        fusion_out = nn.ReLU()(fusion_out)
-        fusion_out = self.dropout(fusion_out)
+            fusion_out = self.fusion_dense2(fusion_out)
+            fusion_out = nn.ReLU()(fusion_out)
+            fusion_out = self.dropout(fusion_out)
         
         logits = self.fc(fusion_out)
 
