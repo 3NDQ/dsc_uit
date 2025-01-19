a2cf5fb7d0ff6550c7446b278dc94f33fe961aed
Gad <faigar1004@gmail.com>
2025-01-19 22:05:59 +0700

    Temp update

--- sarcasm_model.py
+++ sarcasm_model.py
@@ -34,7 +34,7 @@ class VietnameseSarcasmClassifier(nn.Module):
         self.text_dense1 = nn.Linear(1024, 1024)
         self.text_dense2 = nn.Linear(1024, 512)
         
-        self.fusion_dense = nn.Linear(2560, 2048)
+        self.fusion_dense = nn.Linear(3584, 2048)
         self.fusion_dense1 = nn.Linear(2048, 1024)
         self.fusion_dense2 = nn.Linear(1024, 512)
 
@@ -42,12 +42,12 @@ class VietnameseSarcasmClassifier(nn.Module):
         if self.fusion_method == 'cross_attention':
             self.text_to_image_attention = CrossAttention(d_in_q=1024, d_in_kv=2024, d_out_kq=2024, d_out_v=2024)  # d_in for W_query should be 1024 (text_features)
             self.image_to_text_attention = CrossAttention(d_in_q=2024, d_in_kv=1024, d_out_kq=1024, d_out_v=1024)  # d_in for W_query should be 2024 (image_features)
-            combined_size = 2048
+            combined_size = 3584
         elif self.fusion_method == 'attention':
             self.self_attention = SelfAttention(d_in=3584, d_out_kq=2048, d_out_v=2048)
-            combined_size = 2560
+            combined_size = 3584
         else:
-            combined_size = 2560
+            combined_size = 3584
             
         self.fc = nn.Sequential(
             nn.Linear(512, 512 // 2),
@@ -85,7 +85,7 @@ class VietnameseSarcasmClassifier(nn.Module):
         text_out2 = nn.GELU()(text_out2)
         text_out2 = self.dropout(text_out2)
         
-        text_out_combined = torch.cat((text_out1, text_out2), dim=1)
+        text_out_combined = torch.cat((text_out1, text_out2, text_features), dim=1)
         
         if self.fusion_method == 'cross_attention':
             attended_text = self.text_to_image_attention(text_features, image_features)
