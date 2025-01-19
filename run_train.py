cd7d5fbaf5bacc9ba94b8c8734f23e48ed4a9ff0
Gad <faigar1004@gmail.com>
2025-01-19 08:50:14 +0700

    Temp update

--- run_train.py
+++ run_train.py
@@ -16,7 +16,7 @@ import numpy as np
 import json
 from sklearn.utils.class_weight import compute_class_weight
 
-def train_model(model, train_dataloader, val_dataloader, device, num_epochs, patience, learning_rate, class_weights_tensor):
+def train_model(model, train_dataloader, val_dataloader, device, num_epochs, patience, learning_rate):
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
     num_training_steps = len(train_dataloader) * num_epochs
