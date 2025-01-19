bfb126d4398bb035226660b1a3236f4f105b4213
Gad <faigar1004@gmail.com>
2025-01-19 20:35:41 +0700

    Temp update

--- sarcasm_model.py
+++ sarcasm_model.py
@@ -76,7 +76,7 @@ class VietnameseSarcasmClassifier(nn.Module):
         self.class_weight = [0.01, 0.68, 0.3, 0.01]
         logging.info(f"Using class_weight: {self.class_weight}")
         # self.loss_fct = FocalLoss(gamma=self.gamma, alpha=self.class_weight) if self.class_weight is not None else FocalLoss(gamma=self.gamma)
-        self.loss_fct = FocalLoss(gamma=self.gamma, alpha=[0.1, 0.5, 0.3, 0.1])
+        self.loss_fct = FocalLoss(gamma=self.gamma, alpha=self.class_weight)
     def forward(self, image_features, text_features, labels=None):
         # --- "Old" Code Forward Pass ---
         projected_text_features = self.text_projection(text_features)
